d3f6e63c41d34de9fa69481ec0a90ce6a9f33988
Alen Medjimorec <alen.medjimorec@gmail.com>
2023-07-05 17:06:45 +0200

    String Ends With? challenge completed

---
+++ 7kyu/string_ends_with.py
@@ -0,0 +1,5 @@
+# Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+
+def string_ends_with(text, ending):
+    return text.endswith(ending)
