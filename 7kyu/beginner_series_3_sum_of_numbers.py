264fb50a0967d1946d8a69630157c35ec8399b47
Alen Medjimorec <alen.medjimorec@gmail.com>
2023-07-06 10:00:53 +0200

    Beginner Series 3 Sum Of Numbers challenge solved

---
+++ 7kyu/beginner_series_3_sum_of_numbers.py
@@ -0,0 +1,17 @@
+"""
+Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it.
+If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+"""
+
+
+def sum_of_numbers(num_a, num_b):
+    num_sum = 0
+    for i in range(min(num_a, num_b), max(num_a, num_b) + 1):
+        num_sum += i
+    
+    return num_sum
+
+
+print(sum_of_numbers(5, 5))            
