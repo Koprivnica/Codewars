4a2e560198035cf5e2e201c54be558c0d5eb995a
Alen Medjimorec <alen.medjimorec@gmail.com>
2023-07-25 01:12:14 +0200

    Help Suzuki Rake His Garden challenge solved

---
+++ 7kyu/help_suzuki_rake_his_garden.py
@@ -0,0 +1,34 @@
+"""
+Help Suzuki rake his garden!
+The monastery has a magnificent Zen garden made of white gravel and rocks and it is raked diligently everyday by
+the monks. Suzuki having a keen eye is always on the lookout for anything creeping into the garden that must be
+removed during the daily raking such as insects or moss.
+
+You will be given a string representing the garden such as:
+garden = 'gravel gravel gravel gravel gravel gravel gravel gravel gravel rock slug ant gravel gravel snail rock
+gravel gravel gravel gravel gravel gravel gravel slug gravel ant gravel gravel gravel gravel rock slug gravel
+gravel gravel gravel gravel snail gravel gravel rock gravel snail slug gravel gravel spider gravel gravel gravel
+gravel gravel gravel gravel gravel moss gravel gravel gravel snail gravel gravel gravel ant gravel gravel moss
+gravel gravel gravel gravel snail gravel gravel gravel gravel slug gravel rock gravel gravel rock gravel gravel
+gravel gravel snail gravel gravel rock gravel gravel gravel gravel gravel spider gravel rock gravel gravel'
+
+Rake out any items that are not a rock or gravel and replace them with gravel such that:
+garden = 'slug spider rock gravel gravel gravel gravel gravel gravel gravel'
+
+Returns a string with all items except a rock or gravel replaced with gravel:
+garden = 'gravel gravel rock gravel gravel gravel gravel gravel gravel gravel'
+"""
+
+
+def rake_garden(garden):
+    garden_list = garden.split()
+    for i, word in enumerate(garden_list):
+        if word not in ["rock", "gravel"]:
+            garden_list[i] = "gravel"
+    
+    return " ".join(garden_list)
+
+    # also possible
+    # return " ".join["gravel" if word not in ["rock", "gravel"] else word for word in garden.split()]
+
+print(rake_garden("gravel gravel rock gravel gravel gravel gravel gravel gravel gravel"))
