06663d3b15938ebd89e24d7ee672386e7cc470be
Alen Medjimorec <alen.medjimorec@gmail.com>
2023-08-31 23:45:23 +0200

    TMP

--- 7kyu/credit_card_mask.py
+++ 7kyu/credit_card_mask.py
@@ -19,4 +19,4 @@ def maskify(cc):
     return "#" * (len(cc) - 4) + cc[-4:]
 
 
-print(maskify("4556364607935616"))                              
+print(maskify("4556364607935616"))                                               
